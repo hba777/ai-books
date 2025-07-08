f8569f222c766bf57ed344de135c3d63c695badf
Haris Bin Amir <hba777@outlook.com>
2025-07-08 16:51:59 +0500

    Sidebar added to all links

--- frontend/src/pages/classification.tsx
+++ frontend/src/pages/classification.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Sidebar from "@/components/Sidebar/Sidebar";
 
 const Classifcation: React.FC = () => {
     return (
-        <div></div>
+        <div className="min-h-screen flex bg-gradient-to-br from-blue-50 to-purple-100">
+            <Sidebar />
+            <main className="flex-1 flex flex-col items-center py-12 px-4">
+
+            </main>
+        </div>
     )
 }
 
--- frontend/src/pages/reports.tsx
+++ frontend/src/pages/reports.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Sidebar from "@/components/Sidebar/Sidebar";
 
 const Reports: React.FC = () => {
     return (
-        <div></div>
+        <div className="min-h-screen flex bg-gradient-to-br from-blue-50 to-purple-100">
+            <Sidebar />
+            <main className="flex-1 flex flex-col items-center py-12 px-4">
+
+            </main>
+        </div>
+
     )
 }
 
