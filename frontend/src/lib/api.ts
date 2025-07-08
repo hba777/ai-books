a69312fb2659095db36bd641ac50202ea8d88da2
Haris Bin Amir <hba777@outlook.com>
2025-07-08 16:21:14 +0500

    Pycache Removed

--- frontend/src/lib/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // e.g., http://localhost:8000
-  withCredentials: true, // if using cookies
-});
-
-export default api;
