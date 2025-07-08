746a1dd5d7184b1110c1f351d89c3e727898aa61
Haris Bin Amir <hba777@outlook.com>
2025-07-08 16:48:25 +0500

    Placeholder files for links

---
+++ frontend/src/pages/help.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Help: React.FC = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Help;
---
+++ frontend/src/pages/review.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Review: React.FC = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Review;
