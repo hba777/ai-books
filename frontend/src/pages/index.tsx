97b43a316142d16bafa3a795cb96c9f749b35d0d
Haris Bin Amir <hba777@outlook.com>
2025-07-15 11:02:20 +0500

    Fixed Middleware and LoginPage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,23 @@
+import Head from 'next/head';
 import LandingCard from '../features/LandingPage/LandingForm';
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-cover bg-center relative" style={{backgroundImage: 'url("/Landing.jpg")'}}>
-      {/* Overlay */}
-      <div className="absolute inset-0 bg-[#0c1219]/80" />
-      {/* Centered Login Card */}
-      <div className="relative z-10 flex items-center justify-center w-full h-full">
-        <LandingCard />
+    <>
+      <Head>
+        <link rel="preload" as="image" href="/Landing.jpg" />
+      </Head>
+      <div
+        className="min-h-screen flex items-center justify-center bg-cover bg-center relative"
+        style={{ backgroundImage: 'url("/Landing.jpg")' }}
+      >
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-[#0c1219]/80" />
+        {/* Centered Login Card */}
+        <div className="relative z-10 flex items-center justify-center w-full h-full">
+          <LandingCard />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
